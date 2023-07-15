78cc78a35f4ae3f8d95f964839c0789af3a253d6
samet <74676437+sametyorgunn@users.noreply.github.com>
2023-07-16 02:23:15 +0300

    rabbitmq exercies

--- RabbitMq.Publisher/Program.cs
+++ RabbitMq.Publisher/Program.cs
@@ -1,29 +1,131 @@
 ﻿using RabbitMQ.Client;
+using Shared;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+using static RabbitMq.Publisher.Program;
 
 namespace RabbitMq.Publisher
 {
-    internal class Program
+    public class Program
     {
+        public enum Logs
+        {
+            Critical = 1,
+            Error = 2,
+            Warning =3,
+            Info = 4
+        }
         static void Main(string[] args)
         {
+            HeaderExchange();
+        }
+        public static void HeaderExchange()
+        {
             var factory = new ConnectionFactory();
-            factory.Uri = new Uri("amqps://zcjsooff:xTtEdMEjx_LXzSVivzd9vNgbH5F5IA8w@cougar.rmq.cloudamqp.com/zcjsooff ");
+            factory.Uri = new Uri("amqps://ydjrybtk:Pv1BXcTsd8qe6MNRpWJr0rTvZQ4vWidU@cougar.rmq.cloudamqp.com/ydjrybtk");
             using var connection = factory.CreateConnection();
             var channel = connection.CreateModel();
-            channel.QueueDeclare("Hello-queue",true,false,false);
+            channel.ExchangeDeclare("HeaderExchange", durable: true, type: ExchangeType.Headers); 
+            Dictionary<string,Object> headers= new Dictionary<string,Object>();
+            headers.Add("format","pdf");
+            headers.Add("shape","a4");
 
+
+            var product = new Product { Id = 1, Name = "deneme", Price = 10, Stock = 2 };
+            var seralizejsonproduct = JsonSerializer.Serialize(product);
+
+            var properties = channel.CreateBasicProperties();
+            properties.Headers = headers;
+            properties.Persistent = true; //mesajları kalıcı hale getirir.
+            channel.BasicPublish("HeaderExchange",string.Empty,properties,Encoding.UTF8.GetBytes(seralizejsonproduct));
+            Console.WriteLine("mesaj gönderilmiştir");
+
+
+
+        }
+        public static void TopicExchange()
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://ydjrybtk:Pv1BXcTsd8qe6MNRpWJr0rTvZQ4vWidU@cougar.rmq.cloudamqp.com/ydjrybtk");
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.ExchangeDeclare("LogsTopic", durable: true, type: ExchangeType.Topic); //durable true exchange ı saklamasına yarar.
+
+            Random rand = new Random();
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+ 
+                Logs log1 = (Logs)rand.Next(1, 5);
+                Logs log2 = (Logs)rand.Next(1, 5);
+                Logs log3 = (Logs)rand.Next(1, 5);
+
+                var route = $"{log1}.{log2}.{log3}";
+                string message = $"Log-type {log1} + {log2} + {log3}";
+                var messagebody = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("LogsTopic", route, null, messagebody);
+                Console.WriteLine($"Log gönderilmiştir - {message}");
+
+                Console.ReadLine();
+            });
+
+        }
+        public static void DirectExchange()
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://ydjrybtk:Pv1BXcTsd8qe6MNRpWJr0rTvZQ4vWidU@cougar.rmq.cloudamqp.com/ydjrybtk");
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.ExchangeDeclare("Logsdirect", durable: true, type: ExchangeType.Direct); //durable true exchange ı saklamasına yarar.
+
+            Enum.GetNames(typeof(Logs)).ToList().ForEach(x =>
+            {
+                var route = $"route-log{x}";
+
+                var queueName = $"direct-queue-{x}";
+                channel.QueueDeclare(queueName, true, false, false);
+                channel.QueueBind(queueName, "Logsdirect",route, null);
+            });
+
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                Logs log = (Logs)new Random().Next(1, 5);
+                string message = $"Log-type {log}";
+                var messagebody = Encoding.UTF8.GetBytes(message);
+
+                var route = $"route-log{log}";
+                channel.BasicPublish("Logsdirect", route, null, messagebody);
+                Console.WriteLine($"Log gönderilmiştir - {message}");
+            });
+
+            
+
+
+        }
+        public static void FanoutExchange()
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://ydjrybtk:Pv1BXcTsd8qe6MNRpWJr0rTvZQ4vWidU@cougar.rmq.cloudamqp.com/ydjrybtk");
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            /* channel.QueueDeclare("Hello-queue",true,false,false);*/ //publisher tarafından kuyruk olusturulmicak
+            channel.ExchangeDeclare("Logsfanout", durable: true, type: ExchangeType.Fanout); //durable true exchange ı saklamasına yarar.
 
             //coklu mesaj gönderme
             Enumerable.Range(1, 50).ToList().ForEach(x =>
             {
-                string message = $"hello world{x}";
+                string message = $"Log {x}";
                 var messagebody = Encoding.UTF8.GetBytes(message); //messagı byte a çevirdik.
-                channel.BasicPublish(string.Empty, "Hello-queue", null, messagebody);
-                Console.WriteLine($"Mesaj gönderilmiştir - {x}");
+                channel.BasicPublish("Logsfanout", "", null, messagebody);
+                Console.WriteLine($"Loglar gönderilmiştir - {x}");
             });
+            Console.ReadLine();
 
             /*durable yani birinci bool değer eğer proje restart olsa 
              * bile veriler silinmez kaydedilir eğer false olursa 
@@ -50,7 +152,6 @@ namespace RabbitMq.Publisher
             //Console.ReadLine();
 
 
-            
 
         }
     }
--- RabbitMq.Subcriber/Program.cs
+++ RabbitMq.Subcriber/Program.cs
@@ -1,28 +1,103 @@
 ﻿using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using Shared;
 using System;
+using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
+using System.Threading;
 
 namespace RabbitMq.Subcriber
 {
-    internal class Program
+    public class Program
     {
+        //fanout da tüm instancelara aynı mesajlar iletilir 
+        //direct de instaclara değerler pay edilerek gönderilir
         static void Main(string[] args)
         {
+            HeadersExchangeGet();
+        }
+        public static void HeadersExchangeGet()
+        {
             var factory = new ConnectionFactory();
-            factory.Uri = new Uri("amqps://zcjsooff:xTtEdMEjx_LXzSVivzd9vNgbH5F5IA8w@cougar.rmq.cloudamqp.com/zcjsooff ");
+            factory.Uri = new Uri("amqps://ydjrybtk:Pv1BXcTsd8qe6MNRpWJr0rTvZQ4vWidU@cougar.rmq.cloudamqp.com/ydjrybtk");
             using var connection = factory.CreateConnection();
             var channel = connection.CreateModel();
-            channel.QueueDeclare("Hello-queue", true, false, false);
-            var consumer = new EventingBasicConsumer(channel);
 
-            channel.BasicQos(0, 5, true);   /* burda true değer var ise mesajlar subcriberlara toplam değer pay 
+            channel.BasicQos(0, 1, true);
+            var consumer = new EventingBasicConsumer(channel);
+            var queueName = channel.QueueDeclare().QueueName;
+
+            Dictionary<string, Object> headers = new Dictionary<string, Object>();
+            headers.Add("format", "pdf");
+            headers.Add("shape", "a4");
+            headers.Add("x-match", "all");
+            channel.QueueBind(queueName, "HeaderExchange",string.Empty,headers);
+
+            channel.BasicConsume(queueName, false, consumer);
+            Console.WriteLine("loglar dinleniyor.");
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Product product = JsonSerializer.Deserialize<Product>(message);
+                Thread.Sleep(1500);
+                Console.WriteLine($"Gelen message :{product.Id} - {product.Name} - {product.Price} - {product.Stock}");
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+            Console.ReadLine();
+        }
+
+        public static void TopicExchangeGet()
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://ydjrybtk:Pv1BXcTsd8qe6MNRpWJr0rTvZQ4vWidU@cougar.rmq.cloudamqp.com/ydjrybtk");
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.BasicQos(0, 1, true);  
+            var consumer = new EventingBasicConsumer(channel);
+            var queueName = channel.QueueDeclare().QueueName;
+            var route = "*.Error.*";
+            channel.QueueBind(queueName,"LogsTopic",route);
+
+            channel.BasicConsume(queueName, false, consumer);
+            Console.WriteLine("loglar dinleniyor.");
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Thread.Sleep(1500);
+                Console.WriteLine("Gelen message : " + message);
+                channel.BasicAck(e.DeliveryTag, false); 
+            };
+            Console.ReadLine();
+        }
+        public static void DirectExchangeGet()
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://ydjrybtk:Pv1BXcTsd8qe6MNRpWJr0rTvZQ4vWidU@cougar.rmq.cloudamqp.com/ydjrybtk");
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            //channel.ExchangeDeclare("Logs-fanout", durable: true, type: ExchangeType.Fanout); //exchange i burda olustursakta olur olusturmasakta olur 
+            //publisher da zaten olustrduk oyuzden burda olusturmicam 
+
+            //channel.QueueDeclare("Hello-queue", true, false, false);
+
+           
+
+            
+
+
+            channel.BasicQos(0, 1, true);   /* burda true değer var ise mesajlar subcriberlara toplam değer pay 
                                              * edilir örneğin 3 birine 2 birine yada ordaki değeri 6 varsayarsak
                                              * 3 birine 3 birine
-                                             * false ise ordaki değer ne ise o aktarılı yani 5 birine 5 birine
+                                             * false ise ordaki değer ne ise o aktarılı yani 5 birine 5 birine */
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume("direct-queue-Info", false, consumer);
+            Console.WriteLine("loglar dinleniyor.");
 
-
-            channel.BasicConsume("Hello-queue", false,consumer);
+            //channel.QueueBind("direct-queue-Info", "LogsDirect", "route-logInfo", null);
             /*burdaki true olan bool değer mesaj gelir gelmez kuyruktan
               mesajı siler.Eğer false olursa mesajı tutar.
             */
@@ -31,13 +106,55 @@ namespace RabbitMq.Subcriber
             consumer.Received += (object sender, BasicDeliverEventArgs e) =>
             {
                 var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Thread.Sleep(1500);
                 Console.WriteLine("Gelen message : " + message);
-                channel.BasicAck(e.DeliveryTag,false);  //rabbitmq dan gelen tag a göre mesajı inceler
+                channel.BasicAck(e.DeliveryTag, false);  //rabbitmq dan gelen tag a göre mesajı inceler
+                //false olan değer ilgili mesajın değerin silinmesini sağlar true olursa başka mesajlar var ise 
+                //onlarıda sildirir.
+            };
+            Console.ReadLine();
+        }
+       public static void FanoutExchangeGet()
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://ydjrybtk:Pv1BXcTsd8qe6MNRpWJr0rTvZQ4vWidU@cougar.rmq.cloudamqp.com/ydjrybtk");
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            //channel.ExchangeDeclare("Logs-fanout", durable: true, type: ExchangeType.Fanout); //exchange i burda olustursakta olur olusturmasakta olur 
+            //publisher da zaten olustrduk oyuzden burda olusturmicam 
+
+            //channel.QueueDeclare("Hello-queue", true, false, false);
+
+            var randomqueueName = channel.QueueDeclare().QueueName;
+
+            channel.QueueBind(randomqueueName, "Logsfanout", "", null);
+
+
+            channel.BasicQos(0, 1, true);   /* burda true değer var ise mesajlar subcriberlara toplam değer pay 
+                                             * edilir örneğin 3 birine 2 birine yada ordaki değeri 6 varsayarsak
+                                             * 3 birine 3 birine
+                                             * false ise ordaki değer ne ise o aktarılı yani 5 birine 5 birine */
+            var consumer = new EventingBasicConsumer(channel);
+
+
+
+            channel.BasicConsume(randomqueueName, false, consumer);
+            Console.WriteLine("loglar dinleniyor.");
+            /*burdaki true olan bool değer mesaj gelir gelmez kuyruktan
+              mesajı siler.Eğer false olursa mesajı tutar.
+            */
+
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Thread.Sleep(1500);
+                Console.WriteLine("Gelen message : " + message);
+                channel.BasicAck(e.DeliveryTag, false);  //rabbitmq dan gelen tag a göre mesajı inceler
                 //false olan değer ilgili mesajın değerin silinmesini sağlar true olursa başka mesajlar var ise 
                 //onlarıda sildirir.
             };
         }
 
-     
     }
 }
