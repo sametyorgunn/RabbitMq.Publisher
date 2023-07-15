5f1e4a176e5059528df9abcbf2c1605222582f0f
samet <74676437+sametyorgunn@users.noreply.github.com>
2023-07-15 03:40:18 +0300

    Add project files.

---
+++ RabbitMq.Publisher/Program.cs
@@ -0,0 +1,57 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace RabbitMq.Publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://zcjsooff:xTtEdMEjx_LXzSVivzd9vNgbH5F5IA8w@cougar.rmq.cloudamqp.com/zcjsooff ");
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("Hello-queue",true,false,false);
+
+
+            //coklu mesaj gönderme
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                string message = $"hello world{x}";
+                var messagebody = Encoding.UTF8.GetBytes(message); //messagı byte a çevirdik.
+                channel.BasicPublish(string.Empty, "Hello-queue", null, messagebody);
+                Console.WriteLine($"Mesaj gönderilmiştir - {x}");
+            });
+
+            /*durable yani birinci bool değer eğer proje restart olsa 
+             * bile veriler silinmez kaydedilir eğer false olursa 
+             * memoryde tutar ve her defasında silini.*/
+
+            /* ikinci bool değer ise true ise sadece aynı proje
+             * üzerinden mesaj gönderilir kuyruğa.Eğer false ise
+             * diğer projeler üzerindende mesaj gönderilebilir kuyruğa
+             * */
+
+            /* sonucu bool değer true ise son alıcı yani subcriber sistemden
+             * düşerse yada yok olursa otomatik kuyruğu siler.
+             * eğer false ise silmez */
+
+
+
+
+            //tekli mesaj gönderme
+
+            //string message = "helo world";
+            //var messagebody = Encoding.UTF8.GetBytes(message); //messagı byte a çevirdik.
+            //channel.BasicPublish(string.Empty, "Hello-queue", null,messagebody);
+            //Console.WriteLine("Mesaj gönderilmiştir");
+            //Console.ReadLine();
+
+
+            
+
+        }
+    }
+}
---
+++ RabbitMq.Subcriber/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMq.Subcriber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://zcjsooff:xTtEdMEjx_LXzSVivzd9vNgbH5F5IA8w@cougar.rmq.cloudamqp.com/zcjsooff ");
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("Hello-queue", true, false, false);
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicQos(0, 5, true);   /* burda true değer var ise mesajlar subcriberlara toplam değer pay 
+                                             * edilir örneğin 3 birine 2 birine yada ordaki değeri 6 varsayarsak
+                                             * 3 birine 3 birine
+                                             * false ise ordaki değer ne ise o aktarılı yani 5 birine 5 birine
+
+
+            channel.BasicConsume("Hello-queue", false,consumer);
+            /*burdaki true olan bool değer mesaj gelir gelmez kuyruktan
+              mesajı siler.Eğer false olursa mesajı tutar.
+            */
+
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Console.WriteLine("Gelen message : " + message);
+                channel.BasicAck(e.DeliveryTag,false);  //rabbitmq dan gelen tag a göre mesajı inceler
+                //false olan değer ilgili mesajın değerin silinmesini sağlar true olursa başka mesajlar var ise 
+                //onlarıda sildirir.
+            };
+        }
+
+     
+    }
+}
